807b744db56f457592152b84f5d6309d172f8c62
j0h1hb2 <j0h1hb2@homeoffice.wal-mart.com>
2025-07-07 11:28:28 -0500

    fix(utils): init for event loop and renamed queue file avoiding import confusion

--- utils/event.py
+++ utils/event.py
@@ -1,17 +1,28 @@
-from collections
-from typing import TypeVar imprt deque
+import time
+from typing import Callable, TypeVar, Any
 from future import Future
 from task import Task
+from async_queue import Queue
 
 
-T = TypeVar('T')
+T = TypeVar("T")
+MAXSIZE = 1024
 
 
 class EventLoop:
-
-
     def __init__(self) -> None:
-        self._scheduled = []
+        # queue of ready callbacks of the form (callback, args)
+        self._ready: Queue[tuple[Callable[..., Any], tuple[Any, ...]]] = Queue()
+        # scheduled callbacks: min heap of (when, call order, callback, args)
+        self._scheduled: list[
+            tuple[float, int, Callable[..., None], tuple[Any, ...]]
+        ] = []
+        self._seq = 0
+        self._stopped = False
         # TODO: Consider selectors for I/O watchers
 
+    def time(self) -> float:
+        return time.time()
 
+    def call_soon(self, callback: Callable[..., None], *args: Any) -> None:
+        pass
