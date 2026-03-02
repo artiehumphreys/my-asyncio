414990a66c358903878366024ec35403269a1ad6
artiehumphreys <artieh2003@gmail.com>
2026-03-02 14:44:10 -0500

    feat(event loop): run once method

--- src/event_loop.c
+++ src/event_loop.c
@@ -1,6 +1,7 @@
 #include "event_loop.h"
 #include "heap.h"
 
+#include <_time.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
@@ -56,11 +57,45 @@ bool event_loop_call_later(EventLoop *e, PyObject *callback, PyObject *args,
   return true;
 }
 
-double event_loop_time(EventLoop *e) {
+double event_loop_time(const EventLoop *e) {
   struct timespec now;
   if (clock_gettime(CLOCK_MONOTONIC, &now) != 0)
     return -1.0;
 
   double current_time = timespec_to_double(&now);
   return current_time - e->start_time;
+}
+
+bool event_loop_run_once(EventLoop *e) {
+  if (!e->running || heap_size(&e->q) == 0)
+    return false;
+
+  struct timespec now;
+  if (clock_gettime(CLOCK_MONOTONIC, &now) != 0)
+    return false;
+
+  double current_time = timespec_to_double(&now);
+
+  heap_age(&e->q, current_time);
+
+  double next_time = heap_peek_time(&e->q);
+  if (next_time > current_time)
+    return true; // work pending, not ready yet.
+
+  HeapItem item;
+  if (!heap_pop(&e->q, &item))
+    return false;
+
+  PyObject *result = (item.args != NULL)
+                         ? PyObject_Call(item.callback, item.args, NULL)
+                         : PyObject_CallNoArgs(item.callback);
+
+  Py_XDECREF(result);
+  Py_DECREF(item.callback);
+  Py_XDECREF(item.args);
+
+  if (result == NULL)
+    PyErr_Print();
+
+  return e->running;
 }
--- src/event_loop.h
+++ src/event_loop.h
@@ -19,7 +19,7 @@ bool event_loop_call_soon(EventLoop *e, PyObject *callback, PyObject *args,
 bool event_loop_call_later(EventLoop *e, PyObject *callback, PyObject *args,
                            double delay, int16_t priority);
 
-double event_loop_time(EventLoop *e);
+double event_loop_time(const EventLoop *e);
 bool event_loop_run_once(EventLoop *e);
 
 #endif
